756011c9d7c354fde21bdf63b737d46cc8599c56
Tekisatsu <ale.hartikainen@gmail.com>
2026-04-05 15:53:39 +0300

    put and delete routes

--- src/routes/questions.js
+++ src/routes/questions.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const questions = require("../data/questions");
+const req = require("express/lib/request");
 router.get("/", (req, res) => {
       const { keyword } = req.query;
       if (!keyword) {
@@ -20,4 +21,43 @@ router.get("/:id", (req, res) => {
       res.json(question);
 });
 
+router.post("/", (req, res) => {
+      const { question, answer } = req.body;
+      if (!question || !answer) {
+            return res.status(400).json({ msg: "question or answer missing" });
+      }
+      const maxId = Math.max(...questions.map(q => q.id), 0);
+      const newQuestion = {
+            id: questions.length ? maxId + 1 : 1,
+            question, answer
+      };
+      questions.push(newQuestion);
+      res.status(201).json(newQuestion);
+});
+router.put("./:questionId", (req, res) => {
+      const questionId = Number(req.params.id);
+      const { question, answer } = req.body;
+      const q = questions.find((q) => q.id === questionId);
+      if (!q) {
+            return res.status(404).json({ msg: "Question not found" });
+      }
+      if (!question || !answer) {
+            return res.json({ msg: "Question or answer missing" });
+      }
+      q.question = question;
+      q.answer = answer;
+      res.json(q);
+});
+router.delete("./:questionId", (req, res) => {
+      const questionId = Number(req.params.questionId);
+      const questionIndex = questions.find((q) => q.id === questionId);
+      if (questionIndex === -1) {
+            return res.status(404).json({ msg: "Question not found" });
+      }
+      const deleted = questions.splice(questionIndex, 1);
+      res.json({
+            msg: "Question deleted",
+            question: deleted[0]
+      });
+});
 module.exports = router;
